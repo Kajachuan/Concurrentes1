f9ff9b77ce99ec540c33f6313e8a0722af77eaa6
dcassinotti <damiancassinotti.33@gmail.com>
2019-04-09 23:43:34 -0300

    Adding client log

--- Client/ClientApp.cpp
+++ Client/ClientApp.cpp
@@ -5,16 +5,18 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    auto logger = Logger::getInstance("client main");
+    Logger::setLogLevel(DEBUG);
     std::cout << "Insert your command here: ";
     std::string command;
     std::cin >> command;
+    logger->logMessage(DEBUG, "Recieved command: " + command);
     while (command.size() != 5) {
+        logger->logMessage(ERROR, "Command must be 5 chars long");
         std::cout << "Command must be 5 chars long. Insert your new command: ";
         std::cin >> command;
+        logger->logMessage(DEBUG, "Recieved command: " + command);
     }
-    auto logger = Logger::getInstance("client main");
-    Logger::setLogLevel(DEBUG);
-    logger->logMessage(DEBUG, "test");
     auto *client = new ClientController("/tmp/client-portal", "/tmp/portal-client");
     std::cout << client->portal_request(command) << std::endl;
     Logger::endLogger();
