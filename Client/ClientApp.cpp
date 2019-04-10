7cca1615067fabba3cdb65319a8230e69fc262d6
Kajachuan <kevincajachuan@hotmail.com>
2019-04-10 14:36:40 -0300

    Add client help

--- Client/ClientApp.cpp
+++ Client/ClientApp.cpp
@@ -11,15 +11,23 @@ int main(int argc, char *argv[]) {
     std::string command;
 
     while (command!= "end") {
-        std::cout << "Insert your command here or end to finish: ";
+        std::cout << "Insert your command here (type 'help' for information) or 'end' to finish: ";
         std::cin >> command;
-        while (command.size() != 5 and command != "end") {
+        while (command.size() != 5 && command != "end" && command != "help") {
             logger->logMessage(ERROR, "Command must be 5 chars long: " + command);
             std::cout << "Command must be 5 chars long. Insert your new command: ";
             std::cin >> command;
         }
         logger->logMessage(DEBUG, "Recieved command: " + command);
-        if (command!= "end") {
+        if (command == "help") {
+          std::cout << "The command is composed of five joined characters:" << std::endl;
+          std::cout << "\t- The first character can be 'w' (Weather) or 'e' (Exchange)." << std::endl;
+          std::cout << "\t- The second character can be 'r' (Read) or 'w' (Write)." << std::endl;
+          std::cout << "\t- The remaining characters are the code of the city or the currency" << std::endl;
+          std::cout << "\tExample: wrbas (Read weather information of Buenos Aires)" << std::endl;
+        }
+
+        else if (command!= "end") {
             MSRequest message{};
             message.instanceType = CHAR_TO_SERVICE.at(command.at(0));
             message.method = CHAR_TO_METHOD.at(command.at(1));
